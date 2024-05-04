37ee1335ae7d951579405b32b02e1185b4855c72
suron <suron.maharjan@gmail.com>
2024-05-04 13:17:44 +0545

    implement the left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,33 @@
+import Image from "next/image";
+import Sidebar from "../../components/Sidebar";
+import MobileNav from "../../components/MobileNav";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = {firstName: 'Wonjala', lastName: 'Joshi'};
   return (
-   <main>
-    SIDEBAR
-    {children}
+   <main className="flex h-screen w-full font-inter">
+    <Sidebar user={loggedIn}/>
+
+    <div className="flex size-full flex-col">
+      <div className="root-layout">
+        <Image 
+        src="/icons/logo.svg"
+        width={30}
+        height={30}
+        alt="Logo"
+        />
+        <div>
+          <MobileNav user={loggedIn}/>
+        </div>
+      </div>
+      {children}
+    </div>
+    
    </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import HeaderBox from '../../components/HeaderBox';
+import TotalBalanceBox from '../../components/TotalBalanceBox';
 
 const Home = () => {
+
+  const loggedIn = {firstName: 'Wonjala'};
+
   return (
-    <div>page</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your accoutno and transactions effeciently"
+          />
+
+        <TotalBalanceBox 
+          accounts={[]}
+          totalBanks = {1}
+          totalCurrentBalance={1221.27}
+        />      
+        </header>
+      </div>
+    </section>
   )
 }
 
